51da759463d0b193d436fed284cd3de6ad5da2bc
Peter Anema <email@peteranema.nl>
2022-09-01 10:29:26 +0200

    Update

--- Sandbox/competitie.py
+++ Sandbox/competitie.py
@@ -1,14 +1,34 @@
-
+import pickle
 
 class Club:
-    pass
+    def __init__(self, naam, plaats):
+        self.id = 675
+        self.naam = naam
+        self.plaats = plaats
+        self.teams = []
+
+    def team_toevoegen(self, team):
+        self.teams.append(team)
+
+    def store_in_pickle(self, filename = 'club.pickle'):
+        with open(filename, 'wb') as f:
+            pickle.dump(self.__dict__, f)
+
+    def restore_from_pickle(self, filename = 'club.pickle'):
+        with open(filename, 'rb') as f:
+            self.__dict__.update(pickle.load(f))
+
 
 class Team:
-    def __init__(self):
+    def __init__(self, naam):
+        self.id = 23
+        self.club_id = 675
+        self.naam = naam
         self.spelers = []
 
-    def speler_toevoegen(self, speler):
-        self.spelers.append(speler)
+    def speler_toevoegen(self, *spelers):
+        for speler in spelers:
+            self.spelers.append(speler)
 
 class Speler:
     def __init__(self, naam, rugnummer, positie):
@@ -17,6 +37,43 @@ class Speler:
         self.positie = positie
 
 class Wedstrijd:
-    pass
+
+    def __init__(self, datum, plaats, thuis_team, uit_team):
+        self.datum = datum
+        self.plaats = plaats
+        self.thuis_team = thuis_team
+        self.uit_team = uit_team
+        self.thuis_doelpunten = None
+        self.uit_doelpunten = None
+
+    def uitslag(self, thuis_doelpunten, uit_doelpunten):
+        self.thuis_doelpunten = thuis_doelpunten
+        self.uit_doelpunten = uit_doelpunten
 
 #----------------------------------------
+
+club_ADO = Club('ADO', 'Den Haag')
+club_PSV = Club('PSV', 'Eindhoven')
+club_AJAX = Club('Ajax', 'Amsterdam')
+
+team = Team('Eerste elftal')
+
+speler1 = Speler('Johan Cruijf', 14, 'spits')
+speler2 = Speler('Kees', 13, 'keeper')
+speler3 = Speler('Jan', 15, 'verdediger')
+
+team.speler_toevoegen(speler1, speler2, speler3)
+club_ADO.team_toevoegen(team)
+
+
+team = Team('Eerste elftal')
+
+speler1 = Speler('Piet', 9, 'spits')
+speler2 = Speler('van Haneigem', 7, 'keeper')
+speler3 = Speler('Luke', 4, 'verdediger')
+
+team.speler_toevoegen(speler1, speler2, speler3)
+
+club_AJAX.team_toevoegen(team)
+
+print('OK')
--- Sandbox/person.py
+++ Sandbox/person.py
@@ -1,14 +1,25 @@
+from datetime import date
+
 class Person:
 
-    def __init__(self, name, residence):
+    def __init__(self, name, residence, geboortedatum = None):
         self.name = name
         self.residence = residence
+        self.geboortedatum = geboortedatum
 
     def tell(self):
-        return (f'I am {self.name} from {self.residence}')
+        leeftijd_text = f'Ik ben {self.leeftijd}.' if self.leeftijd else 'Leeftijd onbekend.'
+        return (f'I am {self.name} from {self.residence}. {leeftijd_text}')
 
     def move(self, new_residence):
         self.residence = new_residence
+
+    @property
+    def leeftijd(self):
+        if self.geboortedatum is not None:
+            return int((date.today() - date.fromisoformat(self.geboortedatum)).days // 365.25)
+        else:
+            return None
 
 
 # ------------------------------------------------------------
@@ -18,8 +29,8 @@ print(p1.tell())
 p1.move('Urk')
 print(p1.tell())
 
-p2 = Person('Gert Jan', 'Den Haag')
+p2 = Person('Gert Jan', 'Den Haag', '1999-02-25')
 print(p2.tell())
 
-p3 = Person('Gert Jan', 'Den Haag')
+p3 = Person('Gert Jan', 'Den Haag', '1958-02-28')
 print(p3.tell())
